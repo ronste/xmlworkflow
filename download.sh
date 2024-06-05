8c85a04c001def82de5e9348b4ba4c064723b103
ronste <ronald.steffen@fu-berlin.de>
2024-06-05 13:15:17 +0200

    minor update

---
+++ download.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/ronste/xmlworkflow/main/Dockerfile
+wget https://raw.githubusercontent.com/ronste/xmlworkflow/main/Readme.md
+wget https://raw.githubusercontent.com/ronste/xmlworkflow/main/Documentation.md
+wget https://raw.githubusercontent.com/ronste/xmlworkflow/main/podman-run-prod.sh
