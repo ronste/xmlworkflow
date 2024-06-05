6b281da07d1608ef49a228e8a5c5f0ad3be982ef
ronste <ronald.steffen@fu-berlin.de>
2024-06-05 12:39:27 +0000

    update start script

--- xmlworkflow-run-prod.sh
+++ xmlworkflow-run-prod.sh
@@ -1,17 +1,32 @@
+#!/bin/bash
+
+# Set default value
+param=${1:-"xmlworkflow"}
+
+# Function to display help message
+display_help() {
+    echo "Usage: xmlworkflow-run-prod <container-name>"
+    echo "If no parameter is provided, the default container name is 'xmlworkflow'."
+}
+
+# Check if help option is provided
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+    display_help
+    return 0
+fi
+
 mkdir -p work/metadata
 mkdir -p store
 
-#!/bin/bash
-
 if command -v podman &> /dev/null
 then
-    podman run -it --name xmlworkflow -d \
+    podman run -it --name $1 -d \
         -v ./work:/root/xmlworkflow/work \
         -v ./store:/root/xmlworkflow/store \
         xmlworkflow:latest
 elif command -v docker &> /dev/null
 then
-    docker run -it --name xmlworkflow -d \
+    docker run -it --name $1 -d \
         -v ./work:/root/xmlworkflow/work \
         -v ./store:/root/xmlworkflow/store \
         xmlworkflow:latest
