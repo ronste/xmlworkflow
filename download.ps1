839a5c37fb9bf7cffd5c3066f1a63e0a59054bed
ronste <54445456+ronste@users.noreply.github.com>
2026-03-06 14:34:02 +0100

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,5 +1,7 @@
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ronste/xmlworkflow/main/Dockerfile" -OutFile "Dockerfile"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ronste/xmlworkflow/main/Readme.md" -OutFile "Readme.md"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ronste/xmlworkflow/main/Documentation.md" -OutFile "Documentation.md"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ronste/xmlworkflow/main/xmlworkflow-run-prod.sh" -OutFile "xmlworkflow-run-prod.ps1"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ronste/xmlworkflow/main/xmlworkflow-run-prod.ps1" -OutFile "xmlworkflow-run-prod.ps1"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ronste/xmlworkflow/main/runConversionChain.ps1" -OutFile "runConversionChain.ps1"
+
 
